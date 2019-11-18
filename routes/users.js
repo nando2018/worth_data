c8ad9e9aaf5b0982ebe7e26159bf1069c7ffca31
nando2018 <hhrs2011@hotmail.com>
2019-11-17 22:19:31 -0500

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+/*var express = require('express');
+var router = express.Router()
+
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' })
+})
+
+router.get('/register', function (req, res) {
+    res.render('register', { title: 'Register' })
+})
+
+
+router.get('/login', function (req, res) {
+    res.render('login', { title: 'Login' })
+})
+
+module.exports = router*/
